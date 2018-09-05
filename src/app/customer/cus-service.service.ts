2f0f90584512ff79ae12d182a901073861ba3998
aretisd9 <aretisd>
2018-09-05 16:14:24 +0700

    add login form

--- src/app/customer/cus-service.service.ts
+++ src/app/customer/cus-service.service.ts
@@ -1,22 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CusServiceService {
-  constructor(private firebase: AngularFireDatabase) {
-    this.customerList = this.firebase.list('users');
+
+export class CusLoginService {
+  constructor() {
+    // this.customerList = this.firebase.list('users');
   }
+  // customerList: AngularFireList<any>;
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.required)
+  });
+}
+
+@Injectable()
+export class CusRegisService {
   customerList: AngularFireList<any>;
 
-  form = new FormGroup({
-    telNum: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  constructor(public db: AngularFireDatabase) {
+    this.customerList = this.db.list('users');
+  }
+
+  regisForm = new FormGroup({
     email: new FormControl('', Validators.email),
     name: new FormControl('', Validators.required),
-    birthDate: new FormControl('', Validators.required),
-    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+    password: new FormControl('', Validators.required)
   });
 
   getCustomers() {
@@ -26,10 +39,8 @@ export class CusServiceService {
   insertCustomer(users) {
     console.log(users);
     this.customerList.push({
-      tel: users.telNum,
       email: users.email,
       name: users.name,
-      birthDate: users.birthDate,
       password: users.password
     });
   }
