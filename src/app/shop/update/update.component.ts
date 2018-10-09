16701b00c869a250c5d2bee8c64908dcdb5d5d6a
aretisd9 <aretisd>
2018-10-09 17:42:56 +0700

    tslint fix

--- src/app/shop/update/update.component.ts
+++ src/app/shop/update/update.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-update',
@@ -9,12 +11,25 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class UpdateComponent implements OnInit {
 
   updateProcess: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  orderProcess: AngularFireList<any>;
+  orderDetail: AngularFireList<any>;
+  constructor(
+    private fb: FormBuilder,
+    private db: AngularFireDatabase,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
     this.updateProcess = this.fb.group({
       rfidCtrl: ['', Validators.required],
       optCtrl: ['', Validators.required]
     });
+  }
+  update() {
+    this.orderDetail = this.db.list('OrderDetail', rfid => rfid.orderByChild('rfidNum').equalTo(this.updateProcess.value.rfidCtrl));
+    console.log();
+    this.orderProcess = this.db.list('OrderDetail');
+    // const tagNum = this.updateProcess.value.rfidCtrl;
+    // this.orderProcess.update(tagNum, {status: this.updateProcess.value.optCtrl});
   }
 }
